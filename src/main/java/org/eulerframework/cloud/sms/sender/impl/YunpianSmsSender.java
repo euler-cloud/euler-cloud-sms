53a90357d4838a306cea42cf4120028995e4c978
cFrost <cFrost@eulerproject.io>
2018-12-13 00:12:08 +0800

    yunpian改用每次初始化新的client

--- src/main/java/org/eulerframework/cloud/sms/sender/impl/YunpianSmsSender.java
+++ src/main/java/org/eulerframework/cloud/sms/sender/impl/YunpianSmsSender.java
@@ -24,12 +24,10 @@ import java.util.Map;
 import java.util.Set;
 
 public class YunpianSmsSender extends LogSupport implements SmsSender {
-    private YunpianClient yunpianClient;
     private YunpianConfig yunpianConfig;
 
     public YunpianSmsSender(YunpianConfig yunpianConfig) {
         this.yunpianConfig = yunpianConfig;
-        this.yunpianClient = new YunpianClient(this.yunpianConfig.getApiKey()).init();
     }
 
     @Override
@@ -42,12 +40,33 @@ public class YunpianSmsSender extends LogSupport implements SmsSender {
             msg = msg.replace("#" + paramName + "#", params.get(paramName));
         }
 
-        Map<String, String> sendParam = this.yunpianClient.newParam(2);
-        sendParam.put(YunpianClient.MOBILE, mobile);
-        sendParam.put(YunpianClient.TEXT, msg);
-
         this.logger.debug("mobile: {}, msg: {}", mobile, msg);
+        this.sendSms(mobile, msg);
+    }
 
-        this.yunpianClient.sms().single_send(sendParam);
+    private void sendSms(String mobile, String msg) {
+        YunpianClient clnt = null;
+        try {
+            // 初始化clnt,使用单例方式
+            clnt = new YunpianClient(this.yunpianConfig.getApiKey()).init();
+
+            // 发送短信API
+            Map<String, String> param = clnt.newParam(2);
+            param.put(YunpianClient.MOBILE, mobile);
+            param.put(YunpianClient.TEXT, msg);
+            clnt.sms().single_send(param);
+            // Result<SmsSingleSend> r = clnt.sms().single_send(param);
+            // 获取返回结果，返回码:r.getCode(),返回码描述:r.getMsg(),API结果:r.getData(),其他说明:r.getDetail(),调用异常:r.getThrowable()
+
+            // 账户:clnt.user().* 签名:clnt.sign().* 模版:clnt.tpl().*
+            // 短信:clnt.sms().* 语音:clnt.voice().* 流量:clnt.flow().*
+            // 隐私通话:clnt.call().*
+
+        } finally {
+            if (clnt != null) {
+                // 释放clnt
+                clnt.close();
+            }
+        }
     }
 }
